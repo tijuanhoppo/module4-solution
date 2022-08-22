c0a095f4669f3efccc6ebaf5999f31938ff930be
Tijuan Hopkins <110780500+tijuanhoppo@users.noreply.github.com>
2022-08-22 00:38:27 -0400

    Update SpeakHello.js

--- SpeakHello.js
+++ SpeakHello.js
@@ -30,6 +30,6 @@ var speakWord = "Hello";
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
-  window. helloSpeaker =  helloSpeaker;
+  window.helloSpeaker =  helloSpeaker;
   
 })(window);
